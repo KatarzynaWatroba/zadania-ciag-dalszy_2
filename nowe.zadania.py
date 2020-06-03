e5558acb910d064247f542ffd16c3b2f9f58cfd7
KatarzynaWatroba <64368089+KatarzynaWatroba@users.noreply.github.com>
2020-06-03 15:35:41 +0000

    Add files via upload

---
+++ nowe.zadania.py
@@ -0,0 +1,110 @@
+#nowe zadania
+
+"""+100 Zadań z pętli w Python 3 (#1)
+ Zadanie 1 
+
+Proszę za pomocą dowolnej pętli wyświetlić poniższy ciąg liczb:
+
+0
+1
+2
+3
+4
+
+for i in range (5):
+	print(i)"""
+
+
+"""zadanie 2
+Proszę za pomocą pętli wyświetlić ciąg liczb:
+
+6
+12
+18
+24
+30
+
+
+for i in range (6,30):
+	print(i)
+
+a=0
+for i in range(5):
+   a = a + 6
+   print(a)
+
+#zadanie3
+
+for a in range(53,58):
+   print(a)
+
+
+
+
+
+for a in range (67, 89):
+ 	print(a)"""
+
+
+
+
+
+
+
+#zadanie 4
+
+"""Proszę za pomocą pętli i instrukcji if wyświetlić następujący ciąg:
+
+1
+2
+3
+4
+Znalazłem 5!
+6
+7
+
+
+for i in range (8):
+	a = a + 1
+	
+	if i == 5:
+		print("znalazlem 5!")
+	continue
+else:
+	print(i)
+
+#rozwiazanie:
+
+for a in range(7):
+   a = a + 1
+   if a ==5:
+      print("Znalazłem 5!")
+      continue
+   print(a)"""
+
+
+
+
+
+
+#zadanie 5
+"""Proszę znaleźć wszystkie liczby ze przedziału (1,20), które dzielą się bez reszty przez 3.
+
+3
+6
+9
+12
+15
+18"""
+
+for i in range (1,20):
+	if i %3 == 0:
+		print(i)
+
+
+
+
+
+
+
+
